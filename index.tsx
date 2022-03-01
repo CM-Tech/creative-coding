e7abb8eae9fca7491714ceae844425b3f0d5dc7c
Cole Kissane <cole.kissane@gmail.com>
2022-02-28 20:11:39 -0800

    menu

--- index.tsx
+++ index.tsx
@@ -30,39 +30,111 @@ import { ValentinesDay } from "./valentines-day";
 import { VoronoiDiagram } from "./voronoi-diagram";
 import { VoronoiDots } from "./voronoi-dots";
 import { Warpy } from "./warpy";
+import { createSignal } from "solid-js";
+import im from './traffic-dots/README.png?url'
 
-const Default = () => {
+const tileWidth=300;
+const tileHeight=600;
+const ExperimentTile =(props)=>{
+  const selected=()=>Math.round(props.selectedIndex)===props.index;
+  
+  
   return (
-    <div style={{ display: "flex", "flex-direction": "column" }}>
-      <Link href="/attraction">Attraction</Link>
-      <Link href="/boid-beats">Boid Beats</Link>
-      <Link href="/boids">Boids</Link>
-      <Link href="/character-rain">Character Rain</Link>
-      <Link href="/character-type">Character Type</Link>
-      <Link href="/cobwebify">Cobwebify</Link>
-      <Link href="/color-blind">Color Blind</Link>
-      <Link href="/diacritic-sound">Diacritic Sound</Link>
-      <Link href="/fireworks">Fireworks</Link>
-      <Link href="/flocking-dots">Flocking Dots</Link>
-      <Link href="/foosball">Foosball</Link>
-      <Link href="/hex-life">Hex Life</Link>
-      <Link href="/light-beamer">Light Beamer</Link>
-      <Link href="/lights-motion">Lights Motion</Link>
-      <Link href="/monster-sound">Monster Sound</Link>
-      <Link href="/name-rain">Name Rain</Link>
-      <Link href="/neon-airhockey">Neon Airhockey</Link>
-      <Link href="/pen-flame">Pen Flame</Link>
-      <Link href="/pixel-rush">Pixel Rush</Link>
-      <Link href="/plasma-ball">Plasma Ball</Link>
-      <Link href="/prismatic">Prismatic</Link>
-      <Link href="/pulsing-square">Pulsing Square</Link>
-      <Link href="/rainbow-goo">Rainbow Goo</Link>
-      <Link href="/sph-water">SPH Water</Link>
-      <Link href="/traffic-dots">Traffic Dots</Link>
-      <Link href="/valentines-day">Valentines Day</Link>
-      <Link href="/voronoi-diagram">Voronoi Diagram</Link>
-      <Link href="/voronoi-dots">Voronoi Dots</Link>
-      <Link href="/warpy">Warpy</Link>
+    <div style={{background:selected()?"#deddd7":"#e4e4e4",width:tileWidth+"px",height:tileHeight+"px",
+    "box-sizing":"border-box",
+    padding:"10px",
+    }} onClick={()=>props.setSelectedIndex(props.index)}
+    
+    >
+    <h1>{props.title}</h1>
+    <img src={im} style={{width:(tileWidth-20)+"px",height:(tileWidth-20)+"px",background:"white","object-fit":"cover","object-position":"center"}}></img>
+    
+    <h1 ><Link href={props.href} style={{"text-decoration":"none"}}>Go
+    </Link></h1>
+  </div>
+    
+  );
+}
+const Default = () => {
+  const [selectedIndexx,setSelectedIndexx]=createSignal(0);
+  const setSelectedIndex=(v:number)=>setSelectedIndexx(Math.max(0,Math.min(28,v)));
+  const selectedIndex=()=>Math.max(0,Math.min(28,selectedIndexx()));
+  let start={x:0,y:0,sIndex:0};
+  const pointer={x:0,y:0,down:false};
+  const triggerMove=()=>{
+    if(pointer.down){
+    
+    setSelectedIndex(start.sIndex-(pointer.x-start.x)/tileWidth);
+    }
+  };
+  return (
+    <div style={{ background:"#e4e4e4",left:0,position:"relative",top:0,overflow:"hidden",width:"100vw",height:"100vh","touch-action":"none"}}
+    
+    
+    onMouseDown={(e)=>{
+      pointer.x=e.clientX;
+      pointer.y=e.clientY;
+      pointer.down=true;
+      start={x:pointer.x,y:pointer.y,sIndex:selectedIndex()+0};
+    }}
+    onMouseMove={(e)=>{
+      pointer.x=e.clientX;
+      pointer.y=e.clientY;
+      triggerMove();
+    }}
+    onMouseUp={(e)=>{
+      pointer.x=e.clientX;
+      pointer.y=e.clientY;
+      pointer.down=false;
+    }}
+    onTouchStart={(e)=>{
+      pointer.x=e.changedTouches[0].clientX;
+      pointer.y=e.changedTouches[0].clientY;
+      pointer.down=true;
+      start={x:pointer.x,y:pointer.y,sIndex:selectedIndex()+0};
+    }}
+    onTouchMove={(e)=>{
+      pointer.x=e.changedTouches[0].clientX;
+      pointer.y=e.changedTouches[0].clientY;
+
+      triggerMove();
+    }}
+    onTouchEnd={(e)=>{
+      pointer.x=e.changedTouches[0].clientX;
+      pointer.y=e.changedTouches[0].clientY;
+      pointer.down=false;
+    }}>
+    <div style={{ display: "flex", "flex-direction": "row","flex-wrap":"nowrap",left:"50vw",top:"50vh",transform:`translate( -${tileWidth*(selectedIndex()+0.5)}px, -${0.5*tileHeight}px )` ,position:"absolute",transition:"transform 0.125s"}}>
+      <ExperimentTile href="/attraction" title={"Attraction"} index={0} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/boid-beats" title={"Boid Beats"} index={1} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/boids" title={"Boids"} index={2} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/character-rain" title={"Character Rain"} index={3} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/character-type" title={"Character Type"} index={4} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/cobwebify" title={"Cobwebify"} index={5} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/color-blind" title={"Color Blind"} index={6} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/diacritic-sound" title={"Diacritic Sound"} index={7} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/fireworks" title={"Fireworks"} index={8} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/flocking-dots" title={"Flocking Dots"} index={9} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/foosball" title={"Foosball"} index={10} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/hex-life" title={"Hex Life"} index={11} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/light-beamer" title={"Light Beamer"} index={12} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/lights-motion" title={"Lights Motion"} index={13} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/monster-sound" title={"Monster Sound"} index={14} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/name-rain" title={"Name Rain"} index={15} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/neon-airhockey" title={"Neon Airhockey"} index={16} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/pen-flame" title={"Pen Flame"} index={17} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/pixel-rush" title={"Pixel Rush"} index={18} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/plasma-ball" title={"Plasma Ball"} index={19} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/prismatic" title={"Prismatic"} index={20} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/pulsing-square" title={"Pulsing Square"} index={21} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/rainbow-goo" title={"Rainbow Goo"} index={22} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/sph-water" title={"SPH Water"} index={23} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/traffic-dots" title={"Traffic Dots"} index={24} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/valentines-day" title={"Valentines Day"} index={25} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/voronoi-diagram" title={"Voronoi Diagram"} index={26} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/voronoi-dots" title={"Voronoi Dots"} index={27} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+      <ExperimentTile href="/warpy" title={"Warpy"} index={28} selectedIndex={selectedIndex()} setSelectedIndex={setSelectedIndex}/>
+    </div>
     </div>
   );
 };
