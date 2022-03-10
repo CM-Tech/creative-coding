bbf60d7b97e6738cbbde786b1072e9a4b6e6ca78
Cole Kissane <cole.kissane@gmail.com>
2022-03-09 18:24:01 -0800

    feat(character-rain): better input and text alignment

--- character-rain/index.tsx
+++ character-rain/index.tsx
@@ -1,5 +1,5 @@
 import chroma from "chroma-js";
-import { onMount } from "solid-js";
+import { createMemo, onCleanup, onMount } from "solid-js";
 import { BASE_DARK, BASE_LIGHT, CYAN_MUL, MAGENTA_MUL, YELLOW_MUL } from "../shared/constants";
 import { createAnimationFrame, createSizeSignal } from "../utils";
 
@@ -13,6 +13,16 @@ export const CharacterRain = () => {
       e.preventDefault();
     }
   }
+  const area = createMemo(() => width() * height() * dpr() * dpr());
+  const columns = 64; //number of columns for the rain
+  const genDrop = () => {
+    return {
+      y: 0,//Math.random() * c.height,
+      size: (Math.random() * 0.5 + 0.75) * Math.sqrt(area()) / columns,
+      x: Math.random() * c.width,
+      color: [CYAN_MUL, MAGENTA_MUL, YELLOW_MUL][Math.floor(Math.random() * 3)]
+    };
+  };
 
   onMount(() => {
     //making the canvas full screen
@@ -27,19 +37,13 @@ export const CharacterRain = () => {
     let characters = "0123456789-+-+==XX-+-+==XX".split("");
     //converting the string into an array of single characters
 
-    const columns = 100; //number of columns for the rain
     //an array of drops - one per column
     const drops: { x: number; size: number; y: number; color: string }[] = [];
 
     //x below is the x coordinate
     //1 = y co-ordinate of the drop(same for every drop initially)
     for (let x = 0; x < columns; x++)
-      drops[x] = {
-        y: -1,
-        size: (Math.random() + 0.25) * c.width / 100,
-        x: Math.random() * c.width,
-        color: [CYAN_MUL, MAGENTA_MUL, YELLOW_MUL][Math.floor(Math.random() * 3)]
-      };
+      drops[x] = genDrop();
 
     //drawing the characters
 
@@ -58,57 +62,69 @@ export const CharacterRain = () => {
       }
       //looping over drops
       for (let i = 0; i < drops.length; i++) {
-        ctx.font = drops[i].size + `px ${"'Noto Sans Mono'"}`;
+        ctx.font = `${drops[i].size * 5 / 6}px ${"'Noto Sans Mono'"}`;
 
         const text = characters[Math.floor(Math.random() * characters.length)];
-        //x = i*font_size, y = value of drops[i]*font_size
-
+        const rWidth = drops[i].size * 3 / 6;
+        const rHeight = drops[i].size;
         ctx.fillStyle = chroma(BASE_LIGHT).alpha(0.25).hex();
         ctx.globalCompositeOperation = "lighter"
-        ctx.fillRect(drops[i].x, drops[i].y * drops[i].size, drops[i].size, drops[i].size)
+        ctx.fillRect(drops[i].x, drops[i].y * drops[i].size, rWidth, rHeight);
         ctx.fillStyle = drops[i].color;
 
         ctx.globalCompositeOperation = "multiply"
-        ctx.fillRect(drops[i].x, drops[i].y * drops[i].size, drops[i].size, drops[i].size)
+        ctx.fillRect(drops[i].x, drops[i].y * drops[i].size, rWidth, rHeight);
 
         ctx.fillStyle = drops[i].color;
         ctx.globalCompositeOperation = "source-over"
-        ctx.fillText(text, drops[i].x + drops[i].size * 0.2, drops[i].y * drops[i].size + drops[i].size * 1);
+        const tDims = ctx.measureText(text);
+        ctx.fillText(text, drops[i].x + rWidth / 2 - (tDims.width / 2), drops[i].y * drops[i].size + rHeight / 2 + (tDims.actualBoundingBoxAscent - tDims.actualBoundingBoxDescent) / 2);
 
         //sending the drop back to the top randomly after it has crossed the screen
         //adding a randomness to the reset to make the drops scattered on the Y axis
-        if (drops[i].y * drops[i].size > c.height && Math.random() > 0.975) {
-          drops[i].y = -1;
-          drops[i].x = Math.random() * c.width;
+        drops[i].y++;
+        if (drops[i].y * drops[i].size > c.height) {
+          drops[i] = genDrop();
         }
 
-        drops[i].y++;
       }
-      ctx.fillStyle = BASE_LIGHT;
       ctx.globalCompositeOperation = "source-over"
-      ctx.font = `${64}px ${"'Noto Sans Mono'"}`;
-      ctx.fillText("Character Rain", 8, 64 + 8);
+      // ctx.fillStyle = BASE_LIGHT;
+      // ctx.font = `${64}px ${"'Noto Sans Mono'"}`;
+      // ctx.fillText("Character Rain", 8, 64 + 8);
     }
 
     function animloop() {
       timer++;
-      if (timer == speed) {
+      if (timer % speed === 0) {
         draw();
         timer = 0;
       }
     }
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < columns * 2; i++) {
       draw();
     }
     createAnimationFrame(animloop);
   });
+  const pUnit = createMemo(() => Math.min(width(), height()) / 16);
   return (
     <>
-      <div class="well inputs" style={{ bottom: 0, top: "auto", position: "fixed" }}>
-        <input ref={textbox!} placeholder="Rain Content" onkeypress={Check} />
-      </div>
       <canvas ref={c!} style={{ width: "100vw", height: "100vh" }} width={width() * dpr()} height={height() * dpr()} />
+      <div style={{ bottom: 0, top: "auto", position: "fixed", left: 0, right: 0 }}>
+        <input ref={textbox!} placeholder="Rain Content" onkeypress={Check} style={{
+          margin: `${pUnit()}px`,
+          width: `${width() - pUnit() * 2}px`,
+          "font-size": `${pUnit()}px`,
+          padding: `${pUnit() * 0.5}px`,
+          "box-sizing": "border-box",
+          border: 0,
+          outline: 0,
+          "font-family": "'Noto Sans Mono'",
+          background: chroma(BASE_DARK).alpha(0.5).hex(),
+          color: BASE_LIGHT,
+        }} />
+      </div>
     </>
   );
 };
